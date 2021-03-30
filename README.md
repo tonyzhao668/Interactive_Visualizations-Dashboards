8f51c11b0c8d767cbf53a1f77a79d1386c3cdabf
Tony Zhao <tonyzhao668@yahoo.com.au>
2021-03-30 11:32:14 +0800

    finished varsion

--- README.md
+++ README.md
@@ -1,14 +1,21 @@
 # Plot.ly Homework - Belly Button Biodiversity
 
-![Bacteria by filterforge.com](Images/bacteria.jpg)
+By Tony Zhao/DBCUWA finished on 29/03/2020
 
-In this assignment, you will build an interactive dashboard to explore the [Belly Button Biodiversity dataset](http://robdunnlab.com/projects/belly-button-biodiversity/), which catalogs the microbes that colonize human navels.
+Remarks: 
 
-The dataset reveals that a small handful of microbial species (also called operational taxonomic units, or OTUs, in the study) were present in more than 70% of people, while the rest were relatively rare.
+* Each requirment has been fufilled and test running well;
+* Though I am not happy with the general layout, anyway;
+* Thank you for review and comments;
+* Details as follows:
 
+I
 ## Step 1: Plotly
 
+Done!
+
 1. Use the D3 library to read in `samples.json`.
+
 
 2. Create a horizontal bar chart with a dropdown menu to display the top 10 OTUs found in that individual.
 
@@ -18,7 +25,6 @@ The dataset reveals that a small handful of microbial species (also called opera
 
 * Use `otu_labels` as the hovertext for the chart.
 
-  ![bar Chart](Images/hw01.png)
 
 3. Create a bubble chart that displays each sample.
 
@@ -32,21 +38,21 @@ The dataset reveals that a small handful of microbial species (also called opera
 
 * Use `otu_labels` for the text values.
 
-![Bubble Chart](Images/bubble_chart.png)
 
 4. Display the sample metadata, i.e., an individual's demographic information.
 
 5. Display each key-value pair from the metadata JSON object somewhere on the page.
 
-![hw](Images/hw03.png)
+
 
 6. Update all of the plots any time that a new sample is selected.
 
 Additionally, you are welcome to create any layout that you would like for your dashboard. An example dashboard is shown below:
 
-![hw](Images/hw02.png)
 
 ## Advanced Challenge Assignment (Optional)
+
+Done!
 
 The following task is advanced and therefore optional.
 
@@ -56,17 +62,13 @@ The following task is advanced and therefore optional.
 
 * Update the chart whenever a new sample is selected.
 
-![Weekly Washing Frequency Gauge](Images/gauge.png)
+
 
 ## Deployment
 
 Deploy your app to a free static page hosting service, such as GitHub Pages. Submit the links to your deployment and your GitHub repo.
 
-## Hints
-
-* Use `console.log` inside of your JavaScript code to see what your data looks like at each step.
-
-* Refer to the [Plotly.js documentation](https://plot.ly/javascript/) when building the plots.
+Done!
 
 ### About the Data
 
--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -1,19 +1,215 @@
+
+//read in the data and build the test id list, and info group list
 //data = d3.json("data/data.json");
+var mtdata;
+var samples;
 d3.json("samples.json").then((data) => { 
    //console.log(data);
    var id_list = data.names;
    //console.log(id_list);
+   mtdata = data.metadata;
+   samples = data.samples;
 
    test_id_fill(id_list);
-  
-});
-
-function test_id_fill(idlist) {
+   //console.log(mtdata);
+   //console.log(samples);
+   function test_id_fill(idlist) {
     var sele = d3.select("#selDataset");
     sele.html("");
     for (var i = 0; i < idlist.length; i++) {
         var optionrow = sele.append("option");
-        optionrow.text = idlist[i];
+        optionrow.text(idlist[i]);
         optionrow.attr("value", idlist[i]);
     };
-}
+    }
+
+    function fill_info_panel(testid){
+        //console.log(mtdata);
+        console.log(testid);
+        var objr = mtdata.filter(item => parseInt(item.id) === parseInt(testid));
+        var objs = samples.filter(item => parseInt(item.id) === parseInt(testid));
+        var obj = objr[0];
+        var obs = objs[0];
+        //console.log(obj);
+        //console.log(Object.keys(obj));
+    
+        var info_keys = Object.keys(obj);
+        var info = Object.values(obj);
+
+        //console.log(obs);
+
+    
+        var opblock = d3.select("#sample-metadata");
+        opblock.html("");
+        for (var i = 0; i < 7; i++) {
+            oprow = opblock.append("div");
+            oprow.text(`${info_keys[i]} : ${info[i]}`);
+            oprow.attr("value", info_keys[i]);
+        };
+
+
+        function charts_show(obja,objb) {
+        
+            var sam_values = objb.sample_values.slice(0, 10).reverse();
+            var otuids = objb.otu_ids.slice(0, 10).reverse();
+            var otulabels = objb.otu_labels.slice(0, 10).reverse();
+            var ylabel = otuids.map(item => `OTU_${item}`);
+            var wfre = obja.wfreq;
+            
+            //console.log(ylabel);
+
+            // Trace1 for the top 10 OTU Data
+            var trace1 = {
+            x: sam_values,
+            y: ylabel,
+            text: otulabels,
+            name: "Top 10 Otu Facts",
+            type: "bar",
+            orientation: "h"
+            };
+
+            var chartData = [trace1];
+
+            var layout = {
+                title: "Top 10 Otu Facts",
+                xaxis:{title:{text:"Sample_Values"}},
+                yaxis: {title:{text:"OTU_IDS"}},
+
+                margin: {
+                  l: 100,
+                  r: 100,
+                  t: 100,
+                  b: 100
+                }
+              };
+
+            Plotly.newPlot("bar", chartData, layout);
+
+            //bubble workshop
+            var yvalues = objb.sample_values;
+            var xvalues = objb.otu_ids;           
+            var colorlist =[];
+            var opalist = [];
+            var sizelist = [];
+            var lengthh = xvalues.length;
+           
+            for (var i = 0; i < lengthh; i++) {
+                var si = getRandomSize();
+                var co = getRandomColor();
+                var op = getRandomOpacity();
+                colorlist.push(co);
+                opalist.push(op);
+                sizelist.push(si);
+            };
+
+            // console.log(colorlist);
+            // console.log(opalist);
+            // console.log(sizelist);
+
+            var trace2 = {
+                x: xvalues,
+                y: yvalues,
+                mode: 'markers',
+                marker: {
+                  color: colorlist,
+                  opacity: opalist,
+                  size: sizelist
+                }
+              };
+              
+              var data = [trace2];
+              
+              var layout = {
+                title: 'OTU ID vs. Sample_Values',
+                xaxis:{title:{text:"OTU_IDS"}},
+                yaxis: {title:{text:"Sample_values"}},
+                showlegend: false,
+                height: 500,
+                width: 800
+              };
+              
+              Plotly.newPlot('bubble', data, layout);
+
+              function getRandomColor() {              
+                var letters = '0123456789ABCDEF';
+                var color = '#';
+                for (var i = 0; i < 6; i++) {
+                  color += letters[Math.floor(Math.random() * 16)];
+                }
+                //console.log(color);
+                return color;
+                }
+
+              function getRandomSize() {
+                var inisize = 100;
+                var size = Math.floor(Math.random() * inisize) + 1;
+                //console.log(size);
+                return size;
+              }
+
+              function getRandomOpacity() {
+                var opa = Math.random();
+                //console.log(opa);
+                return opa;
+              }
+           
+              var data = [
+                {
+                  domain: { x: [0, 1], y: [0, 1] },
+                  value: wfre || 0,
+                  title: { text: "Weekly Belly Button Washing Frequency" },
+                  type: "indicator",
+                  mode: "gauge+number+delta",
+                  //delta: { reference: 4 },
+                  gauge: {
+                    axis: { range: [null, 10] },
+                    steps: [
+                      { range: [0, 2], color: "#F5FAF9" },
+                      { range: [2, 4], color: "#CAF7F4" },
+                      { range: [4, 6], color: "#9BF3ED" },
+                      { range: [6, 8], color: "#51DBD2" },
+                      { range: [8, 10], color:"#23C7BC" }
+                    ],
+                    threshold: {
+                      line: { color: "red", width: 4 },
+                      thickness: 0.75,
+                      value: 9
+                    }
+                  }
+                }
+              ];
+              
+              var layout = { width: 600, height: 450, margin: { t: 0, b: 0 } };
+              Plotly.newPlot('gauge', data, layout);
+           
+
+            // console.log(si);
+            // console.log(co);
+            // console.log(op);
+            
+        }
+
+        charts_show(obj, obs);
+    }
+
+    function handleSubmit() {
+        // Prevent the page from refreshing
+        d3.event.preventDefault();
+      
+        // Select the input value from the form
+        var inpu = d3.select("#selDataset").node().value;
+        //console.log(inpu);
+      
+        // clear the input value
+        d3.select("#selDataset").node().value = "";
+      
+        // Build the plot with the new input
+        fill_info_panel(inpu);
+      }
+    
+    d3.selectAll("#selDataset").on("change", handleSubmit);
+    
+    
+});
+
+
